b141ecc81f3f8c1d94c42824fa98669f508da808
wizard7558 <56692568+wizard7558@users.noreply.github.com>
2025-01-13 21:48:38 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   images: {
     unoptimized: true,
-  }
+  },
+  // Add this to ensure proper CSS processing
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }
+    return config
+  },
 }
 module.exports = nextConfig
